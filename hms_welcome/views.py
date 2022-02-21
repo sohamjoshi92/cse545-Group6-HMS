e718cd66a3da2fc7a73a4555165c9781430e28dc
Neeraj Sreedhar <61509649+Neeraj-1023@users.noreply.github.com>
2022-02-21 15:02:19 -0700

    Login updates

--- hms_welcome/views.py
+++ hms_welcome/views.py
@@ -38,12 +38,20 @@ def register(request):
 def login_user(request):
     uname = request.POST['uname']
     passw = request.POST['pass']
-    u1 = "nsreedh3"
-    p1 = "123456"
-    res = ""
-    if (uname == u1) and (passw == p1):
-        res = "Login successful."
-    else:
-        res = "Login failed. Wrong username or password."
-    
-    return render(request, "result.html", {'result': res})
+    with open('data.csv','r') as f:
+        flag = -1
+        reader = csv.reader(f,delimiter = ',')
+        for i, line in enumerate(reader):
+            if len(line) == 0:
+                continue
+            if line[2] == uname:
+                flag = 0
+                break
+        
+        if flag == -1:
+            return render(request,'login.html',{'err': "Wrong username or password."})
+        else:
+            if line[3] == passw:
+                return render(request,'result.html',{'result': 'login successful'})
+            else:
+                return render(request,'login.html',{'err': "Wrong username or password"})
