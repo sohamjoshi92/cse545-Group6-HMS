4775999445f3e629ab981f978bc6daf4adb288b9
Soham Joshi <sohamjoshi@MacBook-Air.local>
2022-03-10 19:16:50 -0700

    completed login

--- hms_welcome/urls.py
+++ hms_welcome/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.login, name='login'),
+    path("", views.login),
     path('register',views.register)
 ]
--- hms_welcome/views.py
+++ hms_welcome/views.py
@@ -1,24 +1,37 @@
-from operator import delitem
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 
 # Create your views here.
 
 def login(request):
-    return render(request, 'login.html',{'name': 'Neeraj'})
+    if request.method == 'POST':
+        name = request.POST['uname']
+        password = request.POST['psw']
+        user = authenticate(username = name, password = password)
+        try:
+            if user is not None:
+                #login(request, user)
+                print('Logged in successfully !!')
+            else :
+                print('Invalid login credentials')
+        except Exception as e:
+            print(e)
+    return render(request, 'login.html')
 
 def register(request):
     if request.method == 'POST':
         name = request.POST['name']
         email = request.POST['email']
-        password = request.POST['email']
+        password = request.POST['password']
         repeatpassword = request.POST['re-password']
         try :
             if password == repeatpassword:
+                print('Done')
                 user = User.objects.create_user(name, email, password)
                 user.save()
+                return redirect(login)
         except Exception as e :
-            print(e)
-        return redirect(login)
+            print('Exception occured') #Add the error view redirection here
     return render(request, 'register.html')
